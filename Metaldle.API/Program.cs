b520d6e338b45833b7c1d82a8a76c2a6babfddfe
Augustmunkhaus <Fordemkidz69@gmail.com>
2026-04-02 12:40:25 +0200

    updated band list

--- Metaldle.API/Program.cs
+++ Metaldle.API/Program.cs
@@ -1,9 +1,11 @@
 using System.Text.Json.Serialization;
 using Metaldle.Core.Domain.Services;
 using Metaldle.Infrastructure;
+using Metaldle.infrastructure.Metalbands;
 using Metaldle.Infrastructure.Metalbands;
 using Metaldle.Infrastructure.MetalBands.Seeding;
 using Metaldle.Infrastructure.Redis;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -26,11 +28,15 @@ builder.Services.AddScoped<FeedbackService>();
 builder.Services.AddScoped<GameSessionService>();
 builder.Services.AddScoped<GuessService>();
 builder.Services.AddScoped<GameEngine>();
+builder.Services.AddScoped<DatabaseSeeder>();
 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("BlazorClient", policy =>
-        policy.WithOrigins("http://localhost:5027")
+        policy.WithOrigins(
+                "http://localhost:5027",
+                "http://localhost:80",
+                "http://localhost")
             .AllowAnyMethod()
             .AllowAnyHeader());
 });
@@ -56,6 +62,16 @@ if (args.Contains("--seed"))
     var jsonPath = Path.Combine(AppContext.BaseDirectory, "Seeding", "bands.json");
     await seeder.SeedAsync(jsonPath);
     return;
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await db.Database.MigrateAsync();
+    
+    var seeder = scope.ServiceProvider.GetRequiredService<DatabaseSeeder>();
+    var jsonPath = Path.Combine(AppContext.BaseDirectory, "Seeding", "bands.json");
+    await seeder.SeedAsync(jsonPath);
 }
 
 app.Run();
